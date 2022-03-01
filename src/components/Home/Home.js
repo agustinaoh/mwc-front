21c1405a902d02e77d9d94c984725798ba4d6db9
Silvia <silvia.piovesan@gmail.com>
2022-03-01 13:11:07 +0100

    home text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,14 @@
 function Home() {
   return (
     <>
-      <h1>Welcome Home</h1>
+      <div className="flex flex-col self-start ml-10">
+        <h1 className="text-4xl text-teal-dark mb-5">"Let's bring our net carbon footprint to 0 by 2050" </h1>
+        <h3 className="self-start">-- IATA General Assembly</h3>
+      </div>
+      <div className="flex flex-row ml-40">
+        <button>SEE COMPANIES</button>
+        <button>SEE DATA</button>
+      </div>
     </>
   );
 }
