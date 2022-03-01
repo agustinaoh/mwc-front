9ed6e513c6dc9d824f5e07cc5726f294354d30bb
Silvia <silvia.piovesan@gmail.com>
2022-03-01 14:46:22 +0100

    fix bg navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div className="w-full top-0 fixed p-4 border-b-2 border-teal-dark flex justify-between items-center">
+    <div className="w-full top-0 fixed p-4 border-b-2 border-teal-dark flex justify-between items-center bg-white">
       <Link to="/">
         <img
           src="https://barcelonadigitaltalent.com/app/uploads/sites/3/2020/02/BDT-1.1-POSITIU_2-01.jpg"
