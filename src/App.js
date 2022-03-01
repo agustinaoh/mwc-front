46f71556886f25b61b380a62dba2a56f84aaf4aa
agustina ohan <agustina.oh@gmail.com>
2022-03-01 10:44:31 +0100

    navbar and footer components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Table } from "./components/Table/Table";
+import { Footer } from "./components/Footer/Footer";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="flex flex-col h-full items-center justify-center">
+      <Navbar />
       <Table />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className="w-full bottom-0 fixed p-4 bg-teal-dark text-white"
+    >Footer</div>
+  )
+};
