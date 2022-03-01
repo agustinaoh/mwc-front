6644d5feae7f3e1e57bcd5283dd4f64107f9b246
agustina ohan <agustina.oh@gmail.com>
2022-03-01 06:41:57 +0100

    added colors to tailwind config, imported font families, added react-table and react-icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 module.exports = {
   purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-    './src/**/*.jsx',
-    './src/**/*.ts',
-    './src/**/*.tsx',
-    './public/index.html',
+    "./src/**/*.html",
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.ts",
+    "./src/**/*.tsx",
+    "./public/index.html",
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        "teal-light": "#3FA8C4",
+        "teal-dark": "#116B7D",
+      },
+    },
   },
   variants: {},
-  plugins: []
-}
+  plugins: [],
+};
