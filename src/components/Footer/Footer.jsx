8435eba20cecbe407efbe5d9c61a4faeb37b1729
agustina ohan <agustina.oh@gmail.com>
2022-03-01 14:38:12 +0100

    footer and style on modal

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,70 @@ import React from "react";
 export const Footer = () => {
   return (
     <div className="w-full bottom-0 fixed px-12 py-4 bg-teal-dark text-white">
-      Footer
+      Fast coded by{" "}
+      <a
+        className="mx-2"
+        href="https://github.com/agustinaoh"
+        target={"_blank"}
+        rel="noopener noreferrer"
+      >
+        Agustina
+      </a>
+      |
+      <a
+        className="mx-2"
+        href="https://github.com/silviapio"
+        target={"_blank"}
+        rel="noopener noreferrer"
+      >
+        Silvia
+      </a>
+      |
+      <a
+        className="mx-2"
+        href="https://github.com/sergiogt93"
+        target={"_blank"}
+        rel="noopener noreferrer"
+      >
+        Sergio
+      </a>
+      |
+      <a
+        className="mx-2"
+        href="https://github.com/"
+        target={"_blank"}
+        rel="noopener noreferrer"
+      >
+        Shuangjie
+      </a>
+      |
+      <a
+        className="mx-2"
+        href="https://github.com/"
+        target={"_blank"}
+        rel="noopener noreferrer"
+      >
+        Adrián
+      </a>
+      |
+      <a
+        className="mx-2"
+        href="https://github.com/amda-phd"
+        target={"_blank"}
+        rel="noopener noreferrer"
+      >
+        Adriana
+      </a>
+      |
+      <a
+        className="mx-2"
+        href="https://github.com/"
+        target={"_blank"}
+        rel="noopener noreferrer"
+      >
+        Jordi
+      </a>
+      🚀
     </div>
   );
 };
