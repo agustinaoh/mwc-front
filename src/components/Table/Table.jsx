37b03ce125679b4eadaab38361f32d3ee7ca3f3c
agustina ohan <agustina.oh@gmail.com>
2022-03-01 12:59:34 +0100

    navbar changes

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import {
   useTable,
   usePagination,
@@ -6,8 +6,6 @@ import {
   useGlobalFilter,
   useSortBy,
 } from "react-table";
-import MOCK_DATA from "./helpers/MOCK_DATA.json";
-import { COLUMNS } from "./helpers/columns";
 import { GlobalFilter } from "./helpers/GlobalFilter";
 import "./Table.css";
 import {
@@ -17,10 +15,7 @@ import {
   FaChevronDown,
 } from "react-icons/fa";
 
-export const Table = () => {
-  const columns = useMemo(() => COLUMNS, []);
-  const data = useMemo(() => MOCK_DATA, []);
-
+export const Table = ({ columns, data, onClick }) => {
   const {
     getTableProps,
     getTableBodyProps,
@@ -32,7 +27,6 @@ export const Table = () => {
     canPreviousPage,
     pageOptions,
     gotoPage,
-    setPageSize,
     rows,
     state,
     prepareRow,
@@ -48,7 +42,7 @@ export const Table = () => {
     usePagination
   );
 
-  const { globalFilter, pageIndex, pageSize } = state;
+  const { globalFilter, pageIndex } = state;
 
   const handlePageChange = (value) => {
     let pageNumber = value ? Number(value) - 1 : 0;
@@ -57,82 +51,81 @@ export const Table = () => {
 
   return (
     <>
-      <div className="flex w-full justify-between">
-        <button className="bg-teal-dark py-2 px-6 text-white hover:bg-teal-light">
-          Add new
-        </button>
-        <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
-      </div>
+      <div className="mb-12 w-full">
+        <div className="flex w-full justify-between">
+          <button
+            onClick={onClick}
+            className="bg-teal-dark py-2 px-6 text-white hover:bg-teal-light"
+          >
+            Add new
+          </button>
+          <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+        </div>
 
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  {column.render("Header")}
-                  <span>
-                    {column.isSorted &&
-                      (column.isSortedDesc ? (
-                        <FaChevronDown className="inline ml-2" />
-                      ) : (
-                        <FaChevronUp className="inline ml-2" />
-                      ))}
-                  </span>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {page.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  );
-                })}
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                    {column.render("Header")}
+                    <span>
+                      {column.isSorted &&
+                        (column.isSortedDesc ? (
+                          <FaChevronDown className="inline ml-2" />
+                        ) : (
+                          <FaChevronUp className="inline ml-2" />
+                        ))}
+                    </span>
+                  </th>
+                ))}
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <div className="w-full flex justify-between items-center bg-teal-light bg-opacity-20 py-2 mt-2">
-        <div>
-          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-            <FaChevronLeft />
-          </button>
-        </div>
-        <p>
-          {rows.length} results |{" "}
-          <strong>
-            Page {pageIndex + 1} / {pageOptions.length}
-          </strong>
-        </p>
-        <span>
-          Go to Page
-          <input
-            type="number"
-            defaultValue={pageIndex + 1}
-            onChange={(e) => handlePageChange(e.target.value)}
-            style={{ width: "40px", textAlign: "center" }}
-          />
-        </span>
-        <select value={pageSize} onChange={(e) => setPageSize(e.target.value)}>
-          {[10, 25, 50].map((pageSize) => (
-            <option key={pageSize} value={pageSize}>
-              Show {pageSize}
-            </option>
-          ))}
-        </select>
-        <div>
-          <button onClick={() => nextPage()} disabled={!canNextPage}>
-            <FaChevronRight />
-          </button>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {page.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <div className="w-full flex justify-between items-center bg-teal-light bg-opacity-20 py-2 mt-2">
+          <div>
+            <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+              <FaChevronLeft />
+            </button>
+          </div>
+          <p>
+            {rows.length} results |{" "}
+            <strong>
+              Page {pageIndex + 1} / {pageOptions.length}
+            </strong>
+          </p>
+          <span>
+            Go to Page
+            <input
+              type="number"
+              defaultValue={pageIndex + 1}
+              onChange={(e) => handlePageChange(e.target.value)}
+              style={{ width: "40px", textAlign: "center" }}
+            />
+          </span>
+
+          <div>
+            <button onClick={() => nextPage()} disabled={!canNextPage}>
+              <FaChevronRight />
+            </button>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
