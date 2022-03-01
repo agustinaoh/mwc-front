55fe8abad1ef9ae892a3debb5f2dbbfe42ad360b
Silvia <silvia.piovesan@gmail.com>
2022-03-01 14:04:38 +0100

    home text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ function Home() {
     <>
       <div className="h-full w-full flex justify-center items-center flex-col">
         <h1 className="text-4xl text-teal-dark mb-5">"Let's bring our net carbon footprint to 0 by 2050" </h1>
-        <h3 className="self-start">-- IATA General Assembly</h3>
+        <h3>-- IATA General Assembly</h3>
       </div>
       <div className="flex flex-row ml-40">
         <button>SEE COMPANIES</button>
