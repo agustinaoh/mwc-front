99fc2f3bab62e5da0a9c085227ce5fee2a9fc260
Silvia <silvia.piovesan@gmail.com>
2022-03-01 11:09:29 +0100

    merge router

--- src/components/Companies/Companies.js
+++ src/components/Companies/Companies.js
@@ -3,7 +3,6 @@ import { Table } from "../Table/Table";
 function Companies() {
   return (
     <>
-      <h1>Companies Page</h1>
       <div className="flex flex-col h-full items-center justify-center">
         <Table />
       </div>
