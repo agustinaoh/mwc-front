59fa8f0d62649da467c201b9290956d8f97b6fc3
Silvia <silvia.piovesan@gmail.com>
2022-03-01 12:12:57 +0100

    add home link on BDT logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,8 @@ import {Link} from "react-router-dom"
 export const Navbar = () => {
   return (
     <div className="w-full top-0 fixed p-4 border-b-2 border-teal-dark flex justify-between items-center">
-      <img src="https://barcelonadigitaltalent.com/app/uploads/sites/3/2020/02/BDT-1.1-POSITIU_2-01.jpg" alt="Barcelona Digital Talent" className="h-12"/>
+      <Link to="/">
+      <img src="https://barcelonadigitaltalent.com/app/uploads/sites/3/2020/02/BDT-1.1-POSITIU_2-01.jpg" alt="Barcelona Digital Talent" className="h-12"/></Link>
       <div className="uppercase font-bold text-teal-dark">
       <span className="mr-2"><Link to="/data">Data</Link></span>
       <span><Link to="/companies">Companies</Link></span>
